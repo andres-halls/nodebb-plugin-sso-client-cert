399b78841d84bc0c973c43790e89ae0817229272
andres-liiver <andres.liiver@gmail.com>
2015-11-14 19:04:19 +0200

    Added OCSP check

--- library.js
+++ library.js
@@ -2,50 +2,62 @@
   'use strict';
 
   var user = module.parent.require('./user'),
-    meta = module.parent.require('./meta'),
-    db = module.parent.require('../src/database'),
-    passport = module.parent.require('passport'),
-    passportClientCert = require('passport-client-cert').Strategy,
-    nconf = module.parent.require('nconf'),
-    async = module.parent.require('async'),
-    winston = module.parent.require('winston');
+      meta = module.parent.require('./meta'),
+      db = module.parent.require('../src/database'),
+      passport = module.parent.require('passport'),
+      passportClientCert = require('passport-client-cert').Strategy,
+      nconf = module.parent.require('nconf'),
+      async = module.parent.require('async'),
+      winston = module.parent.require('winston'),
+      fs = require('fs'),
+      openssl = require('openssl-wrapper');
 
   var constants = Object.freeze({
     'name': 'client-cert',
     'icon': 'icon-client-cert-auth'
   });
 
-  var CA_CNs = ['ESTEID-SK 2007', 'ESTEID-SK 2011'];
   // TODO: make a settings page in the Admin CP to configure these
   // and success/failure URLs.
+  const CA_CNS = ['ESTEID-SK 2007', 'ESTEID-SK 2011'];
+  const CERTS_TEMP_FOLDER = 'certs/temp/';
+  const OCSP_URL = 'http://ocsp.sk.ee';
+  const OCSP_INFO = {
+    'ESTEID-SK 2007': {
+      CA_CERT_FILE: 'certs/ESTEID-SK_2007.crt',
+      OCSP_SERVER_CERT_FILE: 'certs/ESTEID-SK_2007_OCSP_RESPONDER_2010.crt'
+    },
+    'ESTEID-SK 2011': {
+      CA_CERT_FILE: 'certs/ESTEID-SK_2011.crt',
+      OCSP_SERVER_CERT_FILE: 'certs/SK_OCSP_RESPONDER_2011.crt'
+    }
+  };
 
   var ClientCert = {};
 
-  ClientCert.getStrategy = function(strategies, callback) {
-    passport.use(constants.name, new passportClientCert({
-        passReqToCallback: true,
-        successReturnToOrRedirect: '/',
-        failureRedirect: '/client-cert-auth-error'
-    }, function(req, cert, done) {
-      var issuer = cert.issuer ? cert.issuer : {CN: undefined};
-      var issuerCommonName = issuer.CN ? issuer.CN : req.headers.ssl_client_i_dn_cn;
+  function verify(req, cert, done) {
+    var subject = cert.subject ? cert.subject : {CN: undefined, GN: undefined, SN: undefined};
+    var subjectCommonName = subject.CN ? subject.CN : req.headers.ssl_client_s_dn_cn;
 
-      if (CA_CNs.indexOf(issuerCommonName) === -1) {
-        winston.error('[sso-client-cert] Client Certificate Issuer CN invalid: ' + issuerCommonName);
-        return done(null, false);
-      }
+    var issuer = cert.issuer ? cert.issuer : {CN: undefined};
+    var issuerCommonName = issuer.CN ? issuer.CN : req.headers.ssl_client_i_dn_cn;
 
-      var subject = cert.subject ? cert.subject : {CN: undefined, GN: undefined, SN: undefined};
-      var subjectCommonName = subject.CN ? subject.CN : req.headers.ssl_client_s_dn_cn;
+    if (!subject) {
+      winston.error('[sso-client-cert] Client Certificate Subject missing!');
+      return done(null, false);
+    } else if (!subjectCommonName) {
+      winston.error('[sso-client-cert] Client Certificate Subject CN missing!');
+      return done(null, false);
+    }
 
-      if (!subject) {
-        winston.error('[sso-client-cert] Client Certificate Subject missing!');
-        return done(null, false);
-      } else if (!subjectCommonName) {
-        winston.error('[sso-client-cert] Client Certificate Subject CN missing!');
-        return done(null, false);
-      }
+    if (CA_CNS.indexOf(issuerCommonName) === -1) {
+      winston.error('[sso-client-cert] Client Certificate Issuer CN invalid: ' + issuerCommonName +
+          '. Subject CN: ' + subjectCommonName);
 
+      return done(null, false);
+    }
+
+    OCSPCheck(cert, subjectCommonName, issuerCommonName, done, function() {
       if (req.hasOwnProperty('user') && req.user.hasOwnProperty('uid') && req.user.uid > 0) {
         // Save cert CN to the user
         user.setUserField(req.user.uid, 'certcn', subjectCommonName);
@@ -71,7 +83,45 @@
 
         done(null, user);
       });
-    }));
+    });
+  }
+
+  function OCSPCheck(cert, subjectCommonName, issuerCommonName, done, callback) {
+    var certFile = CERTS_TEMP_FOLDER + subjectCommonName + '.crt';
+
+    fs.writeFile(certFile, cert, function(err) {
+      if (err) return done(err);
+
+      openssl.exec('ocsp', {
+        url: OCSP_URL,
+        issuer: OCSP_INFO[issuerCommonName].CA_CERT_FILE,
+        VAfile: OCSP_INFO[issuerCommonName].OCSP_SERVER_CERT_FILE,
+        cert: certFile
+      }, function(err, buffer) {
+        fs.unlink(certFile);
+
+        if (err.indexOf & err.indexOf('Response verify OK') === -1) {
+          return done(err);
+        }
+
+        var response = buffer.toString();
+
+        if (response.indexOf('good') === -1) {
+          winston.error('[sso-client-cert] Client Certificate OCSP check failed! CN: ' + subjectCommonName);
+          return done(null, false);
+        } else {
+          return callback();
+        }
+      });
+    });
+  }
+
+  ClientCert.getStrategy = function(strategies, callback) {
+    passport.use(constants.name, new passportClientCert({
+      passReqToCallback: true,
+      successReturnToOrRedirect: '/',
+      failureRedirect: '/client-cert-auth-error'
+    }, verify));
 
     strategies.push({
       name: 'client-cert',
